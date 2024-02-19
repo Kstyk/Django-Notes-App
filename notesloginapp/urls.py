2e4d6689ac3718e797035ab92bf6f81c513318f1
Seweryn Drag <dragseweryn@zohomail.eu>
2024-02-19 08:21:31 +0100

    Podlinkowanie notes/urls.py w głównym pliku urls

--- notesloginapp/urls.py
+++ notesloginapp/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('home/', home, name="home_path"),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('notes/', include('notes.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
